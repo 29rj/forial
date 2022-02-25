8cad07711a77587431766d5ad661f6320c07eafb
29rj <rishabhrr8253@gmail.com>
2022-02-25 16:04:25 +0530

    Users controllers

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const homeControllers = require('../controllers/home_controllers');
-
 router.get('/',homeControllers.home);//callback function is homeControllers.home whenever url is '/'
+
+//Re-routing to another routes
+router.use('/users',require('./users'));
 
 console.log("Router Loaded!!");
 
