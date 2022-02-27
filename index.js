b961fe51d69ef9206959320f3d29dd426a6c9cf8
29rj <rishabhrr8253@gmail.com>
2022-02-27 18:31:55 +0530

    Added Views

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+
+app.set('views','')
+
 app.listen(port , function(err){
     if(err){
         console.log(`Error in connecting: ${err}`);
