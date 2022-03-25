207d7e2314cb2789d3bf065fe401c36ca274193b
29rj <rishabhrr8253@gmail.com>
2022-03-25 18:47:05 +0530

    Added Authentication At Posting

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsControllers = require('../controllers/post_controllers');
 
-router.post('/create',postsControllers.create);
+router.post('/create',passport.checkAuthentication,postsControllers.create);
 
 module.exports = router;
